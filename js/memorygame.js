c374627f410e83e2768c1213e97d421a733cf310
srilathapotnuru <potnuru.srilatha1241@gmail.com>
2024-12-27 03:34:04 +0100

    Added pictures,moves count and timecount code

--- js/memorygame.js
+++ js/memorygame.js
@@ -44,17 +44,16 @@ const timeGenerator = () => {
         seconds = 0;
     }
 
-};
-
 //format time before displaying
-let secondValue = seconds < 10 ? '0${seconds}' : seconds;
-let minutesValue = minutes < 10 ? '0${minutes}' : minutes;
-timeValue.innerHTML = '<span>Time:</span>${minutesValue}:${secondsValue}';
+    let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+    timeValue.innerHTML = `<span>Time:</span>${minutesValue}:${secondsValue}`;
+};
 
 //for calculating moves
 const movesCounter = () => {
     movesCount +=1;
-    moves.innerHTML = '<span>Moves:</span>${movesCount}';
+    moves.innerHTML = `<span>Moves:</span>${movesCount}`;
 };
 
 //pick random objects from the item array
@@ -95,7 +94,72 @@ const matrixGenerator = (cardValues, size = 4) => {
         </div>
         `;
     }
+    //grid
+    gameContainer.style.gridTemplateColumns = `repeat(${size},auto)`;
+
+    //cards
+    cards = document.querySelectorAll(".card-container");
+    cards.forEach((card) => {
+        card.addEventListener("click", () => {
+            if(!card.classList.contains("matched")) {
+                card.classList.add("flipped");
+                if(!firstCard) {
+                    firstCard= card;
+                    firstCardValue = card.getAttribute("dats-card-value");
+                }
+
+            } else {
+                movesCounter();
+                secondCard = card;
+                let secondCardValue = card.getAttribute("data-card-value")
+                if (firstCardValue == secondCardValue) {
+                    firstCard.classList.add("matched")
+                    secondCard.classList.add("matched");
+                    firstCard = false;
+                    winCount += 1;
+                    if(winCount == Math.floor(cardValues.length/2)) {
+                        result.innerHTML = `<h2>You Won</h2>
+                        <h4>Moves: ${movesCount}</h4>`;
+                        stopGame();
+                    }
+                } else {
+                    let[tempFirst, tempSecond] = [firstCard,secondCard];
+                    firstCard = false;
+                    secondCard = false;
+                    let delay =setTimeOut(() => {
+                        tempFirst.classList.remove("flipped");
+                        tempSecond.classList.remove("flipped");
+                    },900);
+                }
+
+            }
+        });
+
+    });
 };
+
+//start game 
+startButton.addEventListener("click", () => {
+    movesCount = 0;
+    seconds =0;
+    minutes = 0;
+    controls.classList.add("hide");
+    stopButton.classList.remove("hide");
+    startButton.classList.add("hide");
+    interval = setInterval(timeGenerator,1000);
+    moves.innerHTML = `<span>Moves:</span> ${movesCount}`;
+    intializer();
+});
+
+//stop game
+stopButton.addEventListener("click",(stopGame = () =>{
+    controls.classList.remove("hide");
+    stopButton.classList.add("hide");
+    startButton.classList.remove("hide");
+    clearInterval(interval);
+    
+ })
+);
 
 //intialize values and function call
 const intializer = () => {
